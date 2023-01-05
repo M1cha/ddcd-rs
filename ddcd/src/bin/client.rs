ba3c8aa32d510f29a25cab4bdcdf2f86f3f17055
Michael Zimmermann <sigmaepsilon92@gmail.com>
2023-01-05 06:33:59 +0100

    update dependencies

--- ddcd/src/bin/client.rs
+++ ddcd/src/bin/client.rs
@@ -1,4 +1,4 @@
-use clap::Clap;
+use clap::Parser as _;
 use ddcd::*;
 use futures::prelude::sink::SinkExt;
 
--- ddcd/src/lib.rs
+++ ddcd/src/lib.rs
@@ -2,14 +2,14 @@ pub const BRIGHTNESS_UP: u8 = 0x00;
 pub const BRIGHTNESS_DOWN: u8 = 0x01;
 pub const INPUT_SOURCE: u8 = 0x02;
 
-#[derive(Debug, serde::Deserialize, serde::Serialize, clap::Clap)]
+#[derive(Debug, serde::Deserialize, serde::Serialize, clap::Parser)]
 pub enum Command {
     BrightnessUp,
     BrightnessDown,
     InputSource { id: u16 },
 }
 
-#[derive(Debug, serde::Deserialize, serde::Serialize, clap::Clap)]
+#[derive(Debug, serde::Deserialize, serde::Serialize, clap::Parser)]
 pub struct SocketPayload {
     pub model: Option<String>,
 
