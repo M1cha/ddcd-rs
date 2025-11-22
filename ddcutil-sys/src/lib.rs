883d9cf4b5d669510cd11e2e6d7058d97fa7fa04
Michael Zimmermann <sigmaepsilon92@gmail.com>
2025-11-22 20:37:44 +0100

    silence unnecessary_transmutes in bindings

--- ddcutil-sys/src/lib.rs
+++ ddcutil-sys/src/lib.rs
@@ -1,6 +1,7 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
+#![allow(unnecessary_transmutes)]
 #![allow(clippy::unreadable_literal)]
 #![allow(clippy::redundant_static_lifetimes)]
 #![allow(clippy::missing_safety_doc)]
